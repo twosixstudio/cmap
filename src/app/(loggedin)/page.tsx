fc5e57ae956493902128ab09e4e83668d00bce1d
Tom Penfold <info@twosix.studio>
2024-09-16 09:02:44 +0100

    test

--- src/app/(loggedin)/page.tsx
+++ src/app/(loggedin)/page.tsx
@@ -1,3 +1,9 @@
+import { ProjectList } from "./projects/_components/project-list/project-list";
+
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <div>
+      <ProjectList />
+    </div>
+  );
 }
